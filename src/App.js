2ee9bbce83e740fb948c01e7e01ef152589ad6a8
Ryley Ameden <rameden@dyn.com>
2017-12-04 11:39:47 -0500

    before state rework

--- src/App.js
+++ src/App.js
@@ -1,21 +1,60 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+import ReactSwap from 'react-swap';
+import { Grid, Navbar, Nav, NavItem, Jumbotron, Button, Col } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+//consts
+const Swap = ReactSwap;
+const Resume = props => <div className="resume" {...props} />;
+const Bio = props => <div className="bio" {...props} />;
+
+class MyResume extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    return(
+      <p>My Resume</p>
     );
   }
 }
 
+class MyBio extends Component {
+  render() {
+    return(
+      <p>My Bio</p>
+    );
+  }
+}
+
+class App extends Component {
+  state = {opened: false};
+  onClick = () => {
+    this.setState({opened: !this.state.opened});
+  };
+
+  render() {
+    return (
+      <div className="App">
+       <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#">React-Bootstrap</a>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Nav>
+          <NavItem eventKey={1} href="#" onClick={this.onClick}>{this.state.opened ? `Bio` : `Resume`}</NavItem>
+        </Nav>
+      </Navbar>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Ameden.me in React</h1>
+        </header>
+        <ReactSwap isSwapped={this.state.opened}>
+          <Bio className="ame-margin-default"><MyBio /></Bio>
+          <Resume className="ame-margin-default"><MyResume /></Resume>
+        </ReactSwap>
+      </div>
+    );
+  }
+}
 export default App;
